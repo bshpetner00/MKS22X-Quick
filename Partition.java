71889e0e177b69986a1363bd1ba724339ab5e34b
bshpetner00 <bshpetner00@stuy.edu>
2019-03-12 14:41:55 -0400

    Initial commit

---
+++ Partition.java
@@ -0,0 +1,6 @@
+public class Partiton {
+	public int partition(int[] data, int start, int end) {
+		int i = (int) Math.random() * (end - start);
+		return i;
+	}
+}
---
+++ PartyDriver.java
@@ -0,0 +1,7 @@
+public class PartyDriver {
+	public static void main(String[] args) {
+		int[] data = new int[] {0,0,0,0,0,0,0};
+		System.out.println("" + partiton(data,0,6));
+		
+	}
+}
